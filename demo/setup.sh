cc2288a5545c45ed0283e32a8b8418d018db6b3d
lifebutler <life.butler.s@gmail.com>
2023-06-15 23:11:01 +0900

    デプロイエラー nodeのバージョン変更とnode-gypのpythonバージョン変更

--- demo/setup.sh
+++ demo/setup.sh
@@ -15,6 +15,6 @@ git config --global url.https://github.com/.insteadOf git://github.com/
 
 #package.jsonに記録されているパッケージをインストール
 yarn install
-(cd client && yarn add nan@latest &&yarn install)
+(cd client && npm config set python python2.7 && yarn install)
 
 ./setup_libs.sh
