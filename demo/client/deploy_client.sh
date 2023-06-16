4e691eab07a713ad759507b6a9e58b660b88171b
lifebutler <life.butler.s@gmail.com>
2023-06-16 22:59:23 +0900

    yarn client起動

--- demo/client/deploy_client.sh
+++ demo/client/deploy_client.sh
@@ -5,8 +5,8 @@ set -e
 # Default to development environment.
 export NODE_ENVIRONMENT=${NODE_ENVIRONMENT:-development}
 
-truffle compile
-truffle migrate
+# truffle compile
+# truffle migrate
 
 # React環境の実行
 react-scripts start
